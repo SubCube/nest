f94cab399fa0a184c92667baa2d930cf7ab49656
Максим Бельфер <maxbelf1@gmail.com>
2022-02-21 17:49:44 +0300

    feat: new configuration

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-	const PORT = process.env.PORT || 5001
+	const PORT = process.env.PORT || 5002
 
 	const app = await NestFactory.create(AppModule)
 
